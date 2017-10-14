d62f3768429d221d485d137a368aa4d288c0f323
Mike Taylor <mike@indexdata.com>
2017-10-14 15:04:22 +0100

    New area, data/nosferatu/
    This is copied and lightly modified from work that I did on porting
    nearly all of Nosferatu to the Games::ScottAdams module.

---
+++ data/nosferatu/nosferatu.sol
@@ -0,0 +1,72 @@
+e
+s
+get rope
+e
+n
+tie rope
+to balcony
+climb rope
+get key 
+climb rope 
+w
+n
+n
+open red
+open blue
+drop key
+go blue
+n
+get bottle
+get ladder
+s
+w
+s
+w
+n
+n
+n
+get spade
+s
+s
+s
+w
+dig
+drop spade
+w
+jump
+n
+fill bottle
+s
+jump
+e
+e
+e
+n
+e
+e
+go red
+n
+go grave
+give water
+drop bottle
+get lamp
+u
+s
+w
+go blue
+w
+s
+w
+w
+w
+s
+get axe
+drop ladder
+climb ladder
+n
+go hole
+e
+throw axe
+at stalactite
+go passage
+### now what?
