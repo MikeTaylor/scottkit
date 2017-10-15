e099d404421701e022196b195240c7237b7f53d3
Mike Taylor <mike@indexdata.com>
2017-10-15 19:03:15 +0100

    Greatly modified to match original game

--- data/nosferatu/nosferatu.sol
+++ data/nosferatu/nosferatu.sol
@@ -1,89 +1,104 @@
 e
 s
-get rope
 e
+get magic
+read magic		Magic word is: 'OVYEZ'
+drop magic
 n
-tie rope
-to balcony
-climb rope
-get key 
-climb rope 
 w
 n
 n
-open red
-open blue
-drop key
-go blue
+get rope
+s
+s
+e
+tie rope
+to rail
+climb rope
+get key			(The scarlet kipper is a red herring)
+climb rope
+w
 n
-get bottle
+get whisky
+s
+s
+drink whisky		(Otherwise the lid is too heavy to move)
+open sarcophagus
+get cross		(Carrying this stops the Count killing me)
+n
+n
+n
+open door
+drop key
+go door			(Just "w" in the original, where a new exit opens)
+get axe
+n
 get ladder
 s
 w
 s
 w
+drop bottle
+drop ladder
+drop cross
 n
 n
-n
+throw axe
+at door
+go door			(Just "n" in the original, where a new exit opens)
 get spade
 s
 s
 s
-w
-dig
 drop spade
-w
-jump
 n
-fill bottle
+n
+get axe
 s
-jump
+s
+get bottle
+w
+n
+w
+chop thicket
+go thicket		(Just "w" in the original, where a new exit opens)
+get water
+s
+get match
+n
+e
+s
 e
 e
 e
 n
 e
 e
-go red
+e
 n
 go grave
-give water
 drop bottle
 get lamp
-u
-s
-w
-go blue
-w
-s
-w
-w
-w
-s
-get axe
-drop ladder
-climb ladder
-n
 go hole
+s
+w
+go door			(Just "w" in the original, where a new exit opens)
+w
+s
+w
+get spade
+get ladder
+w
+dig hole
+drop spade
 e
+get cross
+w
+go pit
+drop ladder
+go tunnel
+light lamp
+drop match
 throw axe
 at stalactite
-go passage
-kill count
-w
-w
-u
-e
-e
-e
-get coins
-w
-n
-e
-e
-go red
-n
-n
-wait
-go bus
-give coins
+go entrance		(Just "n" in the original, where a new exit opens)
